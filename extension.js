eadcc96d1f73b9f44676295cfe7e7cf704c6e1e1
Joshua Adewole <adewole.josh@gmail.com>
2025-07-14 00:41:09 +0100

    feat: begin extension for personal use-case

--- extension.js
+++ extension.js
@@ -3,6 +3,12 @@ const vscode = require("vscode");
 const fs = require("fs");
 const path = require("path");
 
+
+const isWindows = (process.platform == "win32");
+const pythonFolder = (isWindows ? "Scripts" : "bin");
+const pythonName = (isWindows ? "python.exe": "python");
+
+
 /**
  * @param {vscode.ExtensionContext} context
  */
@@ -28,41 +34,89 @@ async function setupPythonEnvironment(editor, pythonApi) {
     const root = path.parse(currentDir).root;
     const currentWorkspaceFolder = vscode.workspace.getWorkspaceFolder(vscode.Uri.file(editor.document.uri.path));
     const currentWorkspaceFolderPath = currentWorkspaceFolder ? currentWorkspaceFolder.uri.path : null;
-    const venvName = vscode.workspace.getConfiguration().get('pythonEnvy.venvName');
+    const venvName = vscode.workspace.getConfiguration().get('pythonEnvyJnr.venvName');
 
-    while (currentDir !== root) {
-        const venvPath = path.join(currentDir, venvName);
+    if (venvName) {
+        while (currentDir !== root) {
+            const venvPath = path.join(currentDir, venvName);
 
-        if (fs.existsSync(venvPath) && fs.lstatSync(venvPath).isDirectory()) {
-            const currentPythonPath =
-                pythonApi.environments.getActiveEnvironmentPath();
-            let pythonPath = path.join(venvPath, "bin", "python");
-            if (!fs.existsSync(pythonPath)) {
-                pythonPath = path.join(venvPath, "Scripts", "python.exe");
-            }
+            if (fs.existsSync(venvPath) && fs.lstatSync(venvPath).isDirectory()) {
+                const currentPythonPath =
+                    pythonApi.environments.getActiveEnvironmentPath();
+                
+                let pythonPath = path.join(venvPath, pythonFolder, pythonName);
 
-            if (currentPythonPath.path !== pythonPath) {
-                try {
-                    await pythonApi.environments.updateActiveEnvironmentPath(pythonPath);
-                    vscode.window.showInformationMessage(
-                        `Python Envy: interpreter set to: ${pythonPath}`
-                    );
-                } catch (error) {
-                    vscode.window.showErrorMessage(
-                        `Python Envy: error setting Python interpreter: ${error.message}`
-                    );
+                if (currentPythonPath.path !== pythonPath) {
+                    try {
+                        await pythonApi.environments.updateActiveEnvironmentPath(pythonPath);
+                        vscode.window.showInformationMessage(
+                            `Python Envy: interpreter set to: ${pythonPath}`
+                        );
+                    } catch (error) {
+                        vscode.window.showErrorMessage(
+                            `Python Envy: error setting Python interpreter: ${error.message}`
+                        );
+                    }
                 }
+                return;
             }
-            return;
-        }
 
-        if (currentDir === currentWorkspaceFolderPath) {
-            break;
-        }
+            if (currentDir === currentWorkspaceFolderPath) {
+                break;
+            }
 
-        currentDir = path.dirname(currentDir);
-        if (currentDir === ".") {
-            currentDir = "";
+            currentDir = path.dirname(currentDir);
+            if (currentDir === ".") {
+                currentDir = "";
+            }
+        }
+    } else {
+
+        while (currentDir !== root) {
+            const workspaceRoot = path.dirname(currentDir);
+            const venvPath = path.join(workspaceRoot, pythonFolder);  // default search curr dir -> my use-case
+            
+            if (fs.existsSync(venvPath) && fs.lstatSync(venvPath).isDirectory()) {
+                const currentPythonPath =
+                pythonApi.environments.getActiveEnvironmentPath();
+                
+                let pythonPath = path.join(workspaceRoot, pythonFolder, pythonName);  // still defaulting
+                // cases
+                const vv = ".venv";
+                const ev = "env";
+
+                if (!fs.existsSync(pythonPath)) {
+                    const cases = [
+                        path.join(workspaceRoot, vv, pythonFolder, pythonName),
+                        path.join(workspaceRoot, ev, pythonFolder, pythonName),
+                    ];
+
+                    pythonPath = cases.find((cas) => fs.existsSync(cas));
+                }
+
+                if (currentPythonPath.path !== pythonPath) {
+                    try {
+                        await pythonApi.environments.updateActiveEnvironmentPath(pythonPath);
+                        vscode.window.showInformationMessage(
+                            `Python Envy Jnr: interpreter set to: ${pythonPath}`
+                        );
+                    } catch (error) {
+                        vscode.window.showErrorMessage(
+                            `Python Envy Jnr: error setting Python interpreter: ${error.message}`
+                        );
+                    }
+                }
+                return;
+            }
+
+            if (currentDir === currentWorkspaceFolderPath) {
+                break;
+            }
+
+            currentDir = path.dirname(currentDir);
+            if (currentDir === ".") {
+                currentDir = "";
+            }
         }
     }
 }
